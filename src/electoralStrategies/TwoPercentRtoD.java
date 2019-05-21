f30c80c94c018e435af40901efa83d19e09b0054
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-21 01:03:19 -0400

    fixed github indent corruption

--- src/electoralStrategies/TwoPercentRtoD.java
+++ src/electoralStrategies/TwoPercentRtoD.java
@@ -11,55 +11,55 @@ import election.ElectionObservable;
 //two percent of republican votes become democrat
 public class TwoPercentRtoD implements ElecReportingBehavior {
 
-			@Override
-			public String calculateWinner(
-					LinkedHashMap<String, LinkedHashMap<String, Integer>> popElectionMap,
-					LinkedHashMap<String, Integer> elecElectionMap) 
+	@Override
+	public String calculateWinner(
+			LinkedHashMap<String, LinkedHashMap<String, Integer>> popElectionMap,
+			LinkedHashMap<String, Integer> elecElectionMap) 
+	{
+		int elecDemVotesTtl =0;
+		int elecRepubVotesTtl=0;
+	        LinkedHashMap<String, LinkedHashMap<String,Integer>> copy =  new LinkedHashMap<String, LinkedHashMap<String,Integer>>();
+	        copy.putAll(popElectionMap);
+		for(Entry<String, LinkedHashMap<String, Integer>> state : copy.entrySet())
+		{
+			LinkedHashMap<String, Integer> parties = state.getValue();
+			int twoPercentR = (int) (parties.get("reppop") * .02);
+			int newR = parties.get("reppop") - twoPercentR;
+			int newD = parties.get("dempop") + twoPercentR;
+					
+					
+			if(newD > newR)
+				elecDemVotesTtl += elecElectionMap.get(state.getKey());
+			else if(newD < newR)	
+				elecRepubVotesTtl += elecElectionMap.get(state.getKey());
+			else if(newD == newR)	
 			{
-				 int elecDemVotesTtl =0;
-				 int elecRepubVotesTtl=0;
-	            LinkedHashMap<String, LinkedHashMap<String,Integer>> copy =  new LinkedHashMap<String, LinkedHashMap<String,Integer>>();
-	            copy.putAll(popElectionMap);
-				for(Entry<String, LinkedHashMap<String, Integer>> state : copy.entrySet())
+				int half;
+				if(elecElectionMap.get(state.getKey()) % 2 == 0)
+			      	{
+				    	half = elecElectionMap.get(state.getKey()) /2;
+					elecDemVotesTtl += half;
+					elecRepubVotesTtl += half;
+				}
+			   	else 
 				{
-					LinkedHashMap<String, Integer> parties = state.getValue();
-					int twoPercentR = (int) (parties.get("reppop") * .02);
-					int newR = parties.get("reppop") - twoPercentR;
-					int newD = parties.get("dempop") + twoPercentR;
-					
-					
-					if(newD > newR)
-						elecDemVotesTtl += elecElectionMap.get(state.getKey());
-					else if(newD < newR)	
-						elecRepubVotesTtl += elecElectionMap.get(state.getKey());
-					else if(newD == newR)	
-					{
-						int half;
-					    if(elecElectionMap.get(state.getKey()) % 2 == 0)
-					    {
-					    	half = elecElectionMap.get(state.getKey()) /2;
-							elecDemVotesTtl += half;
-							elecRepubVotesTtl += half;
-						}
-					    else 
-					    {
-							half = (elecElectionMap.get(state.getKey()) - 1) /2;
-							elecDemVotesTtl += half;
-							elecRepubVotesTtl += half + 1;
-						}
-			    	}
-				}		
+					half = (elecElectionMap.get(state.getKey()) - 1) /2;
+					elecDemVotesTtl += half;
+					elecRepubVotesTtl += half + 1;
+				}
+			 }
+		}		
 			
-				//return winner	
-				String party = " ";
-					if(elecDemVotesTtl > elecRepubVotesTtl)
-					  	party = "the Democrat candidate.";
-					else if(elecDemVotesTtl < elecRepubVotesTtl)
-					 	party = "the Republican candidate.";
-				 	else
-					  		party = ("too close to call.");
-					return party;
-			}
+		//return winner	
+		String party = " ";
+		if(elecDemVotesTtl > elecRepubVotesTtl)
+			party = "the Democrat candidate.";
+		else if(elecDemVotesTtl < elecRepubVotesTtl)
+			party = "the Republican candidate.";
+		else
+			party = ("too close to call.");
+		return party;
+	}
 
 }	
 		
