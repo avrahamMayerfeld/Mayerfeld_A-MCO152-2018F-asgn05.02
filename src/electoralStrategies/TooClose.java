3b38913ba3c73fa8ac7776ee008dcd24b266a0f0
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-21 00:59:46 -0400

    fixed github indent corruption, but looks like there's an extra brace on line 73. program worked in its time, though.

--- src/electoralStrategies/TooClose.java
+++ src/electoralStrategies/TooClose.java
@@ -10,76 +10,78 @@ import base.ElecReportingBehavior;
 	n. Extra vote goes to republican.*/
 public class TooClose implements ElecReportingBehavior{
 @Override
-public String calculateWinner(LinkedHashMap<String, LinkedHashMap<String, Integer>> popElectionMap,
-		LinkedHashMap<String, Integer> elecElectionMap) {
-	int elecDemVotesTtl = 0;
-	int elecRepubVotesTtl= 0;
-	String minStateName = "";
-	int minLead = 0;
-	for(Entry<String, LinkedHashMap<String, Integer>> state : popElectionMap.entrySet())
-	{   
-		int dif = state.getValue().get("dempop") - state.getValue().get("reppop");
-		if(dif > 0 && (minLead == 0 || dif < minLead))
-		{
-			minLead = dif;
-			minStateName = state.getKey();
-		}
-	}
-	
-	
-	for(Entry<String, LinkedHashMap<String, Integer>> state : popElectionMap.entrySet())
-	{
-		
-		if(state.getKey().equals(minStateName))
-		{
-		   int half;
-		   if(elecElectionMap.get(state.getKey()) % 2 == 0) {
-			   half = elecElectionMap.get(state.getKey()) /2;
-			   elecDemVotesTtl += half;
-			   elecRepubVotesTtl += half;
-			  }
-			  else {
-				  half = (elecElectionMap.get(state.getKey()) - 1) /2;
-				  elecDemVotesTtl += half;
-				  elecRepubVotesTtl += half + 1;
-			 }
-		}
-		else 
-		{	
-			LinkedHashMap<String, Integer> parties = state.getValue();
-			if(parties.get("dempop") > parties.get("reppop"))
-				elecDemVotesTtl += elecElectionMap.get(state.getKey());
-			else if(parties.get("dempop") < parties.get("reppop"))	
-				elecRepubVotesTtl += elecElectionMap.get(state.getKey());
-			else if(parties.get("dempop") == parties.get("reppop"))	
+	public String calculateWinner(LinkedHashMap<String, LinkedHashMap<String, Integer>> popElectionMap,
+			LinkedHashMap<String, Integer> elecElectionMap) {
+		int elecDemVotesTtl = 0;
+		int elecRepubVotesTtl= 0;
+		String minStateName = "";
+		int minLead = 0;
+		for(Entry<String, LinkedHashMap<String, Integer>> state : popElectionMap.entrySet())
+		{   
+			int dif = state.getValue().get("dempop") - state.getValue().get("reppop");
+			if(dif > 0 && (minLead == 0 || dif < minLead))
 			{
-				int half;
-			    if(elecElectionMap.get(state.getKey()) % 2 == 0)
-			    {
-			    	half = elecElectionMap.get(state.getKey()) /2;
-					elecDemVotesTtl += half;
-					elecRepubVotesTtl += half;
-				}
-			    else 
-			    {
+				minLead = dif;
+				minStateName = state.getKey();
+			}
+		}
+
+
+		for(Entry<String, LinkedHashMap<String, Integer>> state : popElectionMap.entrySet())
+		{
+
+			if(state.getKey().equals(minStateName))
+			{
+				   int half;
+				   if(elecElectionMap.get(state.getKey()) % 2 == 0) 
+				   {
+					   half = elecElectionMap.get(state.getKey()) /2;
+					   elecDemVotesTtl += half;
+					   elecRepubVotesTtl += half;
+				   }
+				   else 
+				   {
 					half = (elecElectionMap.get(state.getKey()) - 1) /2;
 					elecDemVotesTtl += half;
 					elecRepubVotesTtl += half + 1;
-				}
-	    	}
+				   }
+			}
+			else 
+			{	
+				LinkedHashMap<String, Integer> parties = state.getValue();
+				if(parties.get("dempop") > parties.get("reppop"))
+					elecDemVotesTtl += elecElectionMap.get(state.getKey());
+				else if(parties.get("dempop") < parties.get("reppop"))	
+					elecRepubVotesTtl += elecElectionMap.get(state.getKey());
+				else if(parties.get("dempop") == parties.get("reppop"))	
+				{
+				    int half;
+				    if(elecElectionMap.get(state.getKey()) % 2 == 0)
+				    {
+						half = elecElectionMap.get(state.getKey()) /2;
+						elecDemVotesTtl += half;
+						elecRepubVotesTtl += half;
+				    }
+				    else 
+				    {
+						half = (elecElectionMap.get(state.getKey()) - 1) /2;
+						elecDemVotesTtl += half;
+						elecRepubVotesTtl += half + 1;
+				    }
+			}
 		}
 	}		
 
-	//return winner	
-	String party = " ";
-	if(elecDemVotesTtl > elecRepubVotesTtl)
-	  	party = "the Democrat candidate.";
-	else if(elecDemVotesTtl < elecRepubVotesTtl)
-	 	party = "the Republican candidate.";
-	else
-  		party = ("too close to call.");
-	return party;
-}
+		//return winner	
+		String party = " ";
+		if(elecDemVotesTtl > elecRepubVotesTtl)
+			party = "the Democrat candidate.";
+		else if(elecDemVotesTtl < elecRepubVotesTtl)
+			party = "the Republican candidate.";
+		else
+			party = ("too close to call.");
+		return party;
+	}
 
 }
 
