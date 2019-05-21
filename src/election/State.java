a5e474bf1fa202e421b620227eb78a0f044c18cf
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-21 00:43:36 -0400

    fixed github indent corruption

--- src/election/State.java
+++ src/election/State.java
@@ -11,20 +11,20 @@ public class State {
 	  
 	  private String statename;
 	
-	public State(int democratPopVotes, int republicanPopVotes, int electoralVotes, String statename) {
+	  public State(int democratPopVotes, int republicanPopVotes, int electoralVotes, String statename) {
 		this.statename = statename;
 		votes = new LinkedHashMap<String,Integer>();
 		setStateElection(democratPopVotes, republicanPopVotes, electoralVotes);
-	}
+	  }
 	
-	public void setStateElection(int democratPopVotes, int republicanPopVotes, int electoralVotes) {
+	  public void setStateElection(int democratPopVotes, int republicanPopVotes, int electoralVotes) {
 		this.republicanPopVotes = republicanPopVotes;
 		this.democratPopVotes = democratPopVotes;
 		elecVotes = electoralVotes;
 		votes.put("dempop", democratPopVotes);
 		votes.put("reppop", republicanPopVotes);
 		votes.put("elec", elecVotes);
-	}
-	public LinkedHashMap getVotes() {return votes;}
-	public String getName() {return statename;}
+	  }
+	  public LinkedHashMap getVotes() {return votes;}
+	  public String getName() {return statename;}
 }
