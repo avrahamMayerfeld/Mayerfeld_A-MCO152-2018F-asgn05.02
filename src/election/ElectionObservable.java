0afe1daee9c3fef6cfbbd1cd8492413513c09207
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-21 00:42:26 -0400

    fixed github indent corruption

--- src/election/ElectionObservable.java
+++ src/election/ElectionObservable.java
@@ -9,21 +9,21 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-	public class ElectionObservable extends Observable {
+public class ElectionObservable extends Observable {
 	
        
-	  private  ArrayList<ElectionObserver> observers = new ArrayList<ElectionObserver>();
+      private  ArrayList<ElectionObserver> observers = new ArrayList<ElectionObserver>();
       private  LinkedHashMap <String,LinkedHashMap<String,Integer>> states = new LinkedHashMap<String,LinkedHashMap<String,Integer>>();
 	   
-	   State chicago = new State(0,0,0,"Chicago");
+       State chicago = new State(0,0,0,"Chicago");
        State alabama = new State(0,0,0,"alabama");
        State newyork = new State(0,0,0,"newyork");
        State ohio = new State(0,0,0,"ohio");
        State texas = new State(0,0,0,"texas");
     
-	   public void setAllVotes(int cpd,int cpr,int ce,int apd,int apr,int ae,int npd,
+       public void setAllVotes(int cpd,int cpr,int ce,int apd,int apr,int ae,int npd,
 			   int npr,int ne,int opd,int opr,int oe,int tpd,int tpr,int te) {
-		   chicago.setStateElection(cpd,cpr,ce);
+	       chicago.setStateElection(cpd,cpr,ce);
 	       alabama.setStateElection(apd,apr,ae);
 	       newyork.setStateElection(npd,npr,ne);
 	       ohio.setStateElection(opd,opr,oe);
@@ -41,15 +41,17 @@ import java.util.Map;
 	
 	   public  LinkedHashMap<String,LinkedHashMap<String,Integer>> getAllVotes() {
 		   return states;
-	 }
+	   }
 
 	   public void attach(ElectionObserver electionObserver){
 		   observers.add(electionObserver);		
 	   }
 
-	   public void notifyAllObservers(){
-		   for (ElectionObserver observer : observers) {
+	   public void notifyAllObservers()
+	   {
+		   for (ElectionObserver observer : observers) 
+		   {
 			   observer.update();
-	      }
+	      	   }
 	   } 
 }
